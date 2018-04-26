534353c1538ecba7133dfa92ffc8b17eb5007458
Sasasasajmon <szymonswic@gmail.com>
2018-04-27 01:23:27 +0200

    Initial commit

---
+++ app/src/main/java/com/szymek/stepcounter/MainActivity.java
@@ -0,0 +1,21 @@
+package com.szymek.stepcounter;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+    }
+}
